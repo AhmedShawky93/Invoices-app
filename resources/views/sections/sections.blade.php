7cb4b4bd051bba99a7ae8c320bba5b84e3891bf1
Ahmed SGad <Ahmed.SGad@mansourgroup.com>
2022-08-25 15:54:03 +0200

    first commit

---
+++ resources/views/sections/sections.blade.php
@@ -0,0 +1,116 @@
+@extends('layouts.master')
+@section('css')
+<!-- Internal Data table css -->
+<link href="{{URL::asset('assets/plugins/datatable/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/datatable/css/buttons.bootstrap4.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/datatable/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/datatable/css/jquery.dataTables.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/datatable/css/responsive.dataTables.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/select2/css/select2.min.css')}}" rel="stylesheet">
+@endsection
+@section('title')
+الاقسام
+@stop
+@section('page-header')
+				<!-- breadcrumb -->
+				<div class="breadcrumb-header justify-content-between">
+					<div class="my-auto">
+						<div class="d-flex">
+							<h4 class="content-title mb-0 my-auto">الاعدادات</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ الاقسام</span>
+						</div>
+					</div>
+				</div>
+				<!-- breadcrumb -->
+@endsection
+@section('content')
+		<!-- Basic modal -->
+		<div class="modal" id="modaldemo1">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content modal-content-demo">
+					<div class="modal-header">
+						<h6 class="modal-title">اضافة قسم</h6><button aria-label="Close" class="close" data-dismiss="modal" type="button"><span aria-hidden="true">&times;</span></button>
+					</div>
+					<div class="modal-body">
+						<div class="form-group">
+							<label for="my-input">اسم القسم</label>
+							<input id="section_name" class="form-control" type="text" name="section">
+						</div>
+						<div class="form-group">
+							<label for="my-input">ملاحظات</label>
+							<textarea id="description" class="form-control" type="text" name="description">
+						</div>
+					</div>
+					<div class="modal-footer">
+						<button class="btn ripple btn-primary" type="button">حفظ</button>
+						<button class="btn ripple btn-secondary" data-dismiss="modal" type="button">الغاء</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End Basic modal -->
+				<!-- row -->
+				<div class="row">
+					<div class="col-xl-12">
+						
+						<div class="card mg-b-20">
+							<div class="card-header pb-0">
+								<div class="d-flex justify-content-between">
+									<a class="col-md-2 modal-effect btn btn-outline-primary btn-block" data-effect="effect-scale" data-toggle="modal" href="#modaldemo1">اضافة قسم</a>
+									<i class="mdi mdi-dots-horizontal text-gray"></i>
+								</div>
+							</div>
+							<div class="card-body">
+								<div class="table-responsive">
+									<table id="example" class="table key-buttons text-md-nowrap">
+										<thead>
+											<tr>
+												<th class="border-bottom-0">#</th>
+												<th class="border-bottom-0">اسم القسم </th>
+												<th class="border-bottom-0">الوصف</th>
+												<th class="border-bottom-0"> العمليات</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>1</td>
+												<td>32501254688</td>
+												<td>12-01-2022</td>
+												<td>12-01-2022</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+				</div>
+				<!-- row closed -->
+			</div>
+			<!-- Container closed -->
+		</div>
+		<!-- main-content closed -->
+@endsection
+@section('js')
+<!-- Internal Data tables -->
+<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
+<!-- Internal Modal js-->
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/responsive.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.bootstrap4.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/jszip.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/pdfmake.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/vfs_fonts.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.html5.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.print.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.colVis.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{URL::asset('assets/js/table-data.js')}}"></script>
+<!--Internal  Datatable js -->
+<script src="{{URL::asset('assets/js/modal.js')}}"></script>
+@endsection
